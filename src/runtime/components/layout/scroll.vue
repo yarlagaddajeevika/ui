bcbe83f7a55931f93f6d639d2491f6eb0602dd25
Jeevika Yarlagadda <47218938+yarlagaddajeevika@users.noreply.github.com>
2024-02-01 12:23:32 -0800

    Create scroll.vue

---
+++ src/runtime/components/layout/scroll.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-on:scroll="handleScroll" style="height: 200px; overflow-y: scroll;">
+      <!-- Your content goes here -->
+      <div style="height: 400px;"> <!-- Content to make the container scrollable -->
+        <!-- Your scrollable content goes here -->
+      </div>
+    </div>
+    <p>Scroll position: {{ scrollPosition }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrollPosition: 0
+    };
+  },
+  methods: {
+    handleScroll() {
+      this.scrollPosition = event.target.scrollTop;
+    }
+  }
+};
+</script>
